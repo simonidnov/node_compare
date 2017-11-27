a4a4ebec6df5d369b731dc4597a14ffa375b851b
simonidnov <sdelamarre@idnovant.fr>
2017-11-27 19:05:41 +0100

    save server test

---
+++ server.js
@@ -0,0 +1,14 @@
+/* ---------- DIF STRING TEST --------------- */
+/*
+require('colors')
+var jsdiff = require('diff'),
+    one = 'beep boop',
+    other = 'beep boob blah',
+    diff = jsdiff.diffChars(one, other);
+ 
+diff.forEach(function(part){
+  var color = part.added ? 'green' :
+    part.removed ? 'red' : 'grey';
+  process.stderr.write(part.value[color]);
+});
+*/
