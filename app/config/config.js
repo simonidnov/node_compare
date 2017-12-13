6215d9cd048a72807a43efb8cbad0813e9c5f3dc
idkids-app-28273 <idkids-app-28273@nc3028.nexylan.net>
2017-12-13 15:37:20 +0100

    change config

--- app/config/config.js
+++ app/config/config.js
@@ -6,15 +6,9 @@ module.exports = {
         }
     },
     'database': {
-<<<<<<< HEAD
-        'user':'idkids-app-28273'
-        'password':'gLEv44TTJrw4z5q4'
-	'name':'idkids-app-com'
-=======
         'user':'idkids-app-28273',
         'password':'gLEv44TTJrw4z5q4',
         'name':'mongodb://localhost/idkids-app-com'
->>>>>>> 9e22cc3a81b44c672cf69b5ddf726b22b98f9a51
     }
 };
 //TODO CHECK SSH KEY SIGNING
