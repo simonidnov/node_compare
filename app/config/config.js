6cf29bc1ccdc5023cb453b6da3d2e7038ac7dc5d
simonidnov <sdelamarre@idnovant.fr>
2017-12-13 15:29:02 +0100

    config new database test

--- app/config/config.js
+++ app/config/config.js
@@ -6,7 +6,9 @@ module.exports = {
         }
     },
     'database': {
-        'users':'mongodb://localhost/auth_users'
+        'user':'idkids-app-28273',
+        'password':'gLEv44TTJrw4z5q4',
+        'name':'mongodb://localhost/idkids-app-com'
     }
 };
 //TODO CHECK SSH KEY SIGNING
