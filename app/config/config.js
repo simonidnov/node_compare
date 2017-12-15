1120f6d2dc46844bb1a607a07793e341c7689fbf
idkids-app-28273 <idkids-app-28273@nc3028.nexylan.net>
2017-12-15 09:24:19 +0100

    Ãsave config

--- app/config/config.js
+++ app/config/config.js
@@ -8,7 +8,23 @@ module.exports = {
     'database': {
         'user':'idkids-app-28273',
         'password':'gLEv44TTJrw4z5q4',
-        'name':'mongodb://localhost/idkids-app-com'
+        'name':'mongodb://localhost/idkids-app-com',
+        'users':"mongodb://idkids-app-28273:gLEv44TTJrw4z5q4@localhost:27017/idkids-app-com"
+    },
+    'emailing': {
+        'from': 'idnovant.it@gmail.com',
+        'host': 'smtp.gmail.com', // hostname
+	'secure':true, 
+        'secureConnection': true, // use SSL 
+        'port': 465, // port for secure SMTP 
+        'transportMethod': 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts 
+        'auth': {
+            'user': 'idnovant.it@gmail.com',
+            'pass': 'Idn#2015'
+        },
+    	'tls': {
+    	    'rejectUnauthorized': false
+    	}
     }
 };
 //TODO CHECK SSH KEY SIGNING
