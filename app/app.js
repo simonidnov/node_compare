4eb295e245c303b9d6e395eefb88f27714c1d802
simonidnov <sdelamarre@idnovant.fr>
2018-05-19 11:14:38 +0200

    try session storage secure only

--- app/app.js
+++ app/app.js
@@ -65,7 +65,7 @@ app.listen(9000);
 app.use(session({
     secret: "secret",
     cookie: {
-	      secure: false,
+	      secure: true,
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24
     },
